44e3937035b4fd5d2cbb6a41a1bf00d6a7039798
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-27 19:58:01 +0530

    Update LineComparisonOOP.java

--- LineComparisonOOP.java
+++ LineComparisonOOP.java
@@ -1,26 +1,26 @@
 public class LineComparisonOOP {
 	
-	//variables
-	private double length_of_line;
-	private int x1, x2, y1, y2;
+     //variables
+      private double length_of_line;
+      private int x1, x2, y1, y2;
 
-	//Parameter constructor
-	public LineComparisonOOP(int x1, int y1, int x2, int y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-		}
+     //Parameter constructor
+      public LineComparisonOOP(int x1, int y1, int x2, int y2) {
+      this.x1 = x1;
+      this.y1 = y1;
+      this.x2 = x2;
+      this.y2 = y2;
+	}
 
 	 // calculating length of line 1 and 2
-	public double lengthOfLine() {
-		length_of_line = Math.sqrt( (x2 - x1) * (x2 - x1)  + (y2- y1) * (y2- y1) );
-		return Math.round(length_of_line * 100.0)/100.0;
+    public double lengthOfLine() {
+	length_of_line = Math.sqrt( (x2 - x1) * (x2 - x1)  + (y2- y1) * (y2- y1) );
+	return Math.round(length_of_line * 100.0)/100.0;
 		}
 
 		// Overriding equals() Method
-    public boolean equals(LineComparisonOOP Line_2)
-    {	boolean equalityValue;
+    public boolean equals(LineComparisonOOP Line_2) {
+    	boolean equalityValue;
 
         if(this.lengthOfLine() == Line_2.lengthOfLine())
             equalityValue = true;
@@ -45,9 +45,9 @@ public class LineComparisonOOP {
 
 
 	public static void main(String[] args) {
-		System.out.println( "Welcome to Line Comparison Computation OOP Program" );
+	System.out.println( "Welcome to Line Comparison Computation OOP Program" );
 
-		// Co-ordinates Generation for 4 Points on Lines 1 & 2
+	// Co-ordinates Generation for 4 Points on Lines 1 & 2
         int x1 = (int)(Math.floor(Math.random()*10));
         int y1 = (int)(Math.floor(Math.random()*10));
         int x2 = (int)(Math.floor(Math.random()*10));
@@ -64,8 +64,8 @@ public class LineComparisonOOP {
         System.out.println("length of line 1 is "+Line_1.lengthOfLine() );
         System.out.println("length of line 2 is "+Line_2.lengthOfLine() );
 	
-		boolean equality_check = Line_1.equals(Line_2);
-		int compare_value = Line_1.compareTo(Line_2);
+	boolean equality_check = Line_1.equals(Line_2);
+	int compare_value = Line_1.compareTo(Line_2);
 
         // Display if Two are Equal or Not Equal
         if(equality_check)
